c154eb51fb6c47f7774a1a2b411077290fe204bb
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-01-18 20:35:17 +0100

    CNN Change

--- AthenaK_legacy/kh_build/src/submit.sh
+++ AthenaK_legacy/kh_build/src/submit.sh
@@ -8,9 +8,6 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --time=00:30:00
 
-module purge
-module load gcc/10
-
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
@@ -18,7 +15,7 @@ export OMP_PLACES=threads
 export PYTHONPATH=$PWD/python:$PYTHONPATH
 
 # ---- main run ----
-# ./athena -i kh_cooling_pcunits.athinput -d rc512_256/
+#./athena -i kh_cooling_pcunits.athinput -d rc512_256/
 
 # ---- restart run ----
-./athena -i kh_cooling_pcunits.athinput -d c2512_256/ -r rc512_256/rst/KH.00005.rst
+./athena -i kh_cooling_pcunits.athinput -d c512_256/ -r rc512_256/rst/KH.00005.rst
--- conv_nn/job.sh
+++ conv_nn/job.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=cnn_train
-#SBATCH --partition=p.test
+#SBATCH --partition=p.gpu	
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=40
 #SBATCH --time=00:30:00
 #SBATCH --output=job.log
 #SBATCH --error=job.err
