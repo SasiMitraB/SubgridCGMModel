1a30d5fad72cb4b69767d464dd498cb4f0c42858
dipayan1317 <dipayan.hsms@gmail.com>
2026-01-19 01:45:31 +0100

    c

--- conv_nn/all_flux_cnn.py
+++ conv_nn/all_flux_cnn.py
@@ -19,16 +19,16 @@ resolution = (512, 256)
 downsample = 32
 in_channels = 6
 out_channels = 12
-layer_size1 = 64
-layer_size2 = 128
-layer_size3 = 256
+layer_size1 = 128
+layer_size2 = 256
+layer_size3 = 512
 kernel_size = 5
 num_epochs = 1000
 print_every = 50
 batch_size = 32
 learning_rate = 1e-3
 weight_decay = 1e-4
-dropout_rate = 0.2
+dropout_rate = 0.4
 
 def nn_data(resolution: tuple, downsample: int) -> tuple:
     """ A function to load the data and return the inputs and outputs for the Conv neural network."""
