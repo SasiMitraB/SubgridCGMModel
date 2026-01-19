a9c9e0ac44cdb1b015d979173b052799f1622ab3
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-01-19 12:05:19 +0100

    c

--- data/mocks/mock_sg.py
+++ data/mocks/mock_sg.py
@@ -20,8 +20,8 @@ def divergence(f, dx, dy):
     return dFx_dx + dFy_dy
 
 resolution = (16, 8)
-file_path = f"/ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK_legacy/sd_build/src/hc{resolution[0]}_{resolution[1]}/bin"
-save_path = f"mocks/sg/hc{resolution}/"
+file_path = f"/ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK_legacy/sd_build/src/clipc{resolution[0]}_{resolution[1]}/bin"
+save_path = f"mocks/sg/clipc{resolution}/"
 os.makedirs(save_path, exist_ok=True)
 
 sim_data = simulation_data()
