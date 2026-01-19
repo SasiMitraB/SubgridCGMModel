c2997880ad7f6639caa8611e117566f6d72559d4
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-01-19 17:21:38 +0100

    c

--- AthenaK/build/kokkos/containers/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK/kokkos/containers
-# Build directory: /ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK/build/kokkos/containers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- AthenaK/build/kokkos/core/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK/kokkos/core/src
-# Build directory: /ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK/build/kokkos/core/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
