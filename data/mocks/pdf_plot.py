439f0a2e75aa69309c1e58edcc50faeb811cfa5c
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-03-21 01:34:02 +0100

    c

--- data/mocks/pdf_plot.py
+++ data/mocks/pdf_plot.py
@@ -17,7 +17,7 @@ from data_preprocess import simulation_data
 # =========================
 resolution = (512, 256)
 downsample = 32
-bins = 200
+bins = 20
 
 folder_path = f"/ptmp/mpa/dipda/subgrid/SubgridCGMModel/AthenaK_legacy/datafiles/c{resolution}_128"
 
