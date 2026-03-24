256d53015a17c18055886b039732a2bc64519bdb
Dipayan Datta <dipayan.hsms@gmail.com>
2026-03-24 06:42:55 +0530

    PDF

--- conv_nn/pdf_cnn.py
+++ conv_nn/pdf_cnn.py
@@ -19,16 +19,16 @@ resolution = (512, 256)
 downsample = 32
 in_channels = 6
 out_channels = 40
-layer_size1 = 32
-layer_size2 = 64
-layer_size3 = 128
+layer_size1 = 64
+layer_size2 = 128
+layer_size3 = 256
 kernel_size = 5
 num_epochs = 1000
 print_every = 50
 batch_size = 64
 learning_rate = 1e-3
 weight_decay = 1e-3
-dropout_rate = 0.1
+dropout_rate = 0.3
 
 def nn_data(resolution: tuple, downsample: int) -> tuple:
     """ A function to load the data and return the inputs and outputs for the Conv neural network."""
