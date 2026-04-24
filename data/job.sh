bf7e180f032a87b48bb50617bf337f4c0118650a
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-04-24 08:25:18 +0200

    c

--- data/job.sh
+++ data/job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=pdf_plot
-#SBATCH --partition=p.test	
+# #SBATCH --partition=p.test	
 # #SBATCH --partition=p.gpu
 # #SBATCH --gres=gpu:2
-# #SBATCH --partition=p.gpu.ampere
-# #SBATCH --gres=gpu:a100:4
+#SBATCH --partition=p.gpu.ampere
+#SBATCH --gres=gpu:a100:4
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
