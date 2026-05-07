2b44e6400aa88ae34ad4be7ea1b928e801e1d2d9
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-05-07 11:37:12 +0200

    T

--- data/job.sh
+++ data/job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=pdf_plot
-#SBATCH --partition=p.24h
+# #SBATCH --job-name=pdf_plot
+# #SBATCH --partition=p.24h
 # #SBATCH --partition=p.test	
-# #SBATCH --partition=p.gpu
-# #SBATCH --gres=gpu:2
+#SBATCH --partition=p.gpu
+#SBATCH --gres=gpu:2
 # #SBATCH --partition=p.gpu.ampere
 # #SBATCH --gres=gpu:a100:4
 #SBATCH --nodes=1
@@ -13,20 +13,20 @@
 #SBATCH --output=job.log
 #SBATCH --error=job.err
 
-rm mocks/pdf/pdf_animation.mp4
-rm mocks/pdf/pdf_compare_animation.mp4
+rm mocks/pdf/log_pdf_animation.mp4
+rm mocks/pdf/log_pdf_compare_animation.mp4
 
 echo "Starting SG Anim..."
-python3 -u mocks/pdf_plot.py
+python3 -u mocks/log_plot.py
 
 echo "Converting gif to mp4..."
 module load ffmpeg
-ffmpeg -i mocks/pdf/pdf_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/pdf_animation.mp4
-ffmpeg -i mocks/pdf/pdf_compare_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/pdf_compare_animation.mp4
+ffmpeg -i mocks/pdf/pdf_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/log_pdf_animation.mp4
+ffmpeg -i mocks/pdf/pdf_compare_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/log_pdf_compare_animation.mp4
 
 echo "Deleting GIFs..."
-rm mocks/pdf/pdf_animation.gif mocks/pdf/pdf_animation.gif
-rm mocks/pdf/pdf_animation.gif mocks/pdf/pdf_compare_animation.gif
+rm mocks/pdf/pdf_animation.gif mocks/pdf/log_pdf_animation.gif
+rm mocks/pdf/pdf_animation.gif mocks/pdf/log_pdf_compare_animation.gif
 
 echo "Conversion done."
 
