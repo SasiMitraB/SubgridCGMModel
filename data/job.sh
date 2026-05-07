4375895286a2e41a695bcd31692de3987ef711d3
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-05-07 12:54:41 +0200

    T

--- data/job.sh
+++ data/job.sh
@@ -21,12 +21,12 @@ python3 -u mocks/log_plot.py
 
 echo "Converting gif to mp4..."
 module load ffmpeg
-ffmpeg -i mocks/pdf/pdf_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/log_pdf_animation.mp4
-ffmpeg -i mocks/pdf/pdf_compare_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/log_pdf_compare_animation.mp4
+ffmpeg -i mocks/pdf/log_pdf_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/log_pdf_animation.mp4
+ffmpeg -i mocks/pdf/log_pdf_compare_animation.gif -movflags faststart -pix_fmt yuv420p mocks/pdf/log_pdf_compare_animation.mp4
 
 echo "Deleting GIFs..."
-rm mocks/pdf/pdf_animation.gif mocks/pdf/log_pdf_animation.gif
-rm mocks/pdf/pdf_animation.gif mocks/pdf/log_pdf_compare_animation.gif
+# rm mocks/pdf/pdf_animation.gif mocks/pdf/log_pdf_animation.gif
+# rm mocks/pdf/pdf_animation.gif mocks/pdf/log_pdf_compare_animation.gif
 
 echo "Conversion done."
 
