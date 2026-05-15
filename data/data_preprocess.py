70e0d4b9b9e4a7d3a7b79825f827eb9688d93edb
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-05-16 01:03:52 +0200

    C

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -98,7 +98,7 @@ class simulation_data():
 
         cwd = os.getcwd()
         os.chdir(filepath)
-        num_snaps = len([f for f in os.listdir(filepath) if f.endswith('.bin')])//2
+        num_snaps = 500 # len([f for f in os.listdir(filepath) if f.endswith('.bin')])//2
 
         self.rho = np.zeros((num_snaps, self.resolution[0], self.resolution[1]))
         self.temp = np.zeros_like(self.rho)
@@ -134,7 +134,7 @@ class simulation_data():
 
         cwd = os.getcwd()
         os.chdir(filepath)
-        num_snaps = len([f for f in os.listdir(filepath) if f.endswith('.bin')])//2
+        num_snaps = 500 # len([f for f in os.listdir(filepath) if f.endswith('.bin')])//2
 
         self.cons_rho = np.zeros((num_snaps, self.resolution[0], self.resolution[1]))
         self.cons_momx = np.zeros_like(self.cons_rho)
