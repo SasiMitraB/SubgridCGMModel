c3080675756f4be753025ba63448463290807275
Dipayan Datta <dipayan.hsms@gmail.com>
2026-05-29 09:44:02 +0530

    Build Instructions

---
+++ AthenaK_legacy/laptop_build_hr/kokkos/algorithms/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dipay/SubgridCGMModel/AthenaK_legacy/kokkos/algorithms/src
+# Build directory: /home/dipay/SubgridCGMModel/AthenaK_legacy/laptop_build_hr/kokkos/algorithms/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
